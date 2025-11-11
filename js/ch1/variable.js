d308c7279dcfcfc5b4f7e8abe85d2436cb9ebc69
BaekSeungCheol <kyn04241@gmail.com>
2025-11-11 17:55:35 +0900

    11.11

---
+++ js/ch1/variable.js
@@ -0,0 +1,16 @@
+// 타입 지정은 따로 안함
+// 변수선언 : var, let
+// 상수선언 : const
+
+var value1 = 4;
+var value2 = 5;
+console.log(value1 + value2);
+
+var value2 = 6;
+console.log(value2);
+
+let value1 = 7;
+console.log(value1);
+
+// 'const' declarations must be initialized
+const value4 = 15;
---
+++ js/ch1/variable2.js
@@ -0,0 +1,11 @@
+// 함수 == 메소드
+function name(params) {
+  var foo = 5;
+  console.log(foo);
+}
+
+if (true) {
+  let a = 5;
+  console.log(a);
+}
+console.log(a);
